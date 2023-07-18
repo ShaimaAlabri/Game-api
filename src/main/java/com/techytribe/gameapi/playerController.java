28fc03b651d13c95ac9e8d959bff864b894a8ef9
unknown <shaima96alabri@hotmail.com>
2023-07-18 00:59:22 -0700

    creat player class

--- src/main/java/com/techytribe/gameapi/playerController.java
+++ src/main/java/com/techytribe/gameapi/playerController.java
@@ -1,16 +1,19 @@
 package com.techytribe.gameapi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api/v1/player")
 public class playerController {
+
+
+    @GetMapping
     public String getHomePageMessage(){
         return "MY HOME PAGE";
     }
+    @PostMapping
     public String yetAnotherMessage(){
         return "Some other message";
     }
-    
+
 }
