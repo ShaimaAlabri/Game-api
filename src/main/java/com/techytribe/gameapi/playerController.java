f89ac4b35466d2995cbe87941ab8e1365d020007
unknown <shaima96alabri@hotmail.com>
2023-07-18 00:44:09 -0700

    Adding getHomePageMessage method in playerController class

--- src/main/java/com/techytribe/gameapi/playerController.java
+++ src/main/java/com/techytribe/gameapi/playerController.java
@@ -1,4 +1,12 @@
 package com.techytribe.gameapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/v1/player")
 public class playerController {
+    public String getHomePageMessage(){
+        return "MY HOME PAGE";
+    }
 }
